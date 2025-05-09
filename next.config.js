514f23855c6c38e9d7a4594dff8b253c870137de
RishabhPrabhu5 <103843699+RishabhPrabhu5@users.noreply.github.com>
2025-05-09 19:49:02 -0400

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
+  basePath: '/RPwebsite',
 }
 
 module.exports = nextConfig 
